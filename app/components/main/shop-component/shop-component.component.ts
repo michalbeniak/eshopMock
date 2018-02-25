e8d2c4912a72959a409102dff972fd83b04a32d5
Michal Beniak <michal.beniak@gmail.com>
2018-02-25 20:54:11 +1100

    Implement sorting in eshop

--- app/components/main/shop-component/shop-component.component.ts
+++ app/components/main/shop-component/shop-component.component.ts
@@ -20,16 +20,16 @@ export class ShopComponentComponent implements OnInit {
   onChange(event) {
     console.log(event);
     if (event.target.value =="priceLowHigh") {
-
+      this.sortLowHigh();
     }
     else if (event.target.value =="priceHighLow") {
-
+      this.sortHighLow();
     }
     else if (event.target.value =="nameAz") {
       this.sortAz();
     }
     else if (event.target.value =="nameZa") {
-      
+      this.sortZa();
     }
     else if (event.target.value =="newest") {
       
@@ -38,7 +38,7 @@ export class ShopComponentComponent implements OnInit {
   }
 
   sortAz(){
-    var sortedArray1:Product[]= this.productList.ProductsList.sort((n1,n2) => {
+    this.productList.ProductsList = this.productList.ProductsList.sort((n1,n2) => {
       if (n1.Name > n2.Name) {
           return 1;
       }
@@ -50,21 +50,43 @@ export class ShopComponentComponent implements OnInit {
       return 0;
   });
 
+  }
 
-
-    var stringArray: string[] = ['AB', 'Z', 'A', 'AC'];
-
-var sortedArray: string[] = stringArray.sort((n1,n2) => {
-    if (n1 > n2) {
-        return 1;
-    }
-
-    if (n1 < n2) {
-        return -1;
-    }
-
-    return 0;
-});
+  sortZa(){
+    this.productList.ProductsList = this.productList.ProductsList.sort((n1,n2) => {
+      if (n1.Name < n2.Name) {
+          return 1;
+      }
+  
+      if (n1.Name > n2.Name) {
+          return -1;
+      }
+  
+      return 0;
+  });
 
   }
+
+  sortHighLow(){
+    this.productList.ProductsList = this.productList.ProductsList.sort((n1,n2) => {
+      if (n1.Price < n2.Price) {
+          return 1;
+      }
+      if (n1.Price > n2.Price) {
+          return -1;
+      }
+      return 0;
+  });
+  }
+  sortLowHigh(){
+  this.productList.ProductsList = this.productList.ProductsList.sort((n1,n2) => {
+    if (n1.Price > n2.Price) {
+        return 1;
+    }
+    if (n1.Price < n2.Price) {
+        return -1;
+    }
+    return 0;
+});
+}
 }
